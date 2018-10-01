f9d5458d60939d0f19200e312344f4ef8f43f69f
AndersonMarquess <andersongabriel96.ag@gmail.com>
2018-10-01 12:51:15 -0300

    Commit Inicial

---
+++ Curso2/OrientacaoObjetos/OrientacaoObjetos/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace OrientacaoObjetos
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            ContaCorrente cc = new ContaCorrente(123,98521);
+            cc.Saldo = 10;
+
+            //Ou
+
+            ContaCorrente cc2 = new ContaCorrente(123, 98522) {
+                Saldo = 20
+            };
+
+            Console.WriteLine("Saldo: " + cc.Saldo + ", Conta: " + cc.Conta);
+            Console.WriteLine("Saldo: "+cc2.Saldo+", Conta: "+cc2.Conta);
+
+            Console.WriteLine("Total de contas: " + ContaCorrente.TotalDeContasCriadas);
+
+            Console.Read();
+        }
+    }
+}

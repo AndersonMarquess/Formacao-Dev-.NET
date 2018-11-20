b718c5721b52a56bba4e3ebca443587ad47820bf
AndersonMarquess <andersongabriel96.ag@gmail.com>
2018-11-20 20:55:50 -0200

    Simplificando a leitura com o StreamReader

--- Curso9/IO/IO/Program.cs
+++ Curso9/IO/IO/Program.cs
@@ -10,28 +10,17 @@ namespace IO
             //Pega o caminho relativo à aplicação, dentro da pasta bin/debug/Dados.txt
             var filePath = "Dados.txt";
 
-            //using == try with resources
-            using (FileStream fluxoDoArquivo = new FileStream(filePath, FileMode.Open)) {
-                var buffer = new byte[128];
-                var numeroDeBytesLidos = -1;
 
-                while(numeroDeBytesLidos != 0) {
-                    numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 128);
-                    printBuffer(buffer, numeroDeBytesLidos);
+            using(var fluxoDoArquivo = new FileStream(filePath, FileMode.Open))
+            using(var leitor = new StreamReader(fluxoDoArquivo)) {
+                while(!leitor.EndOfStream) {
+                    var linha = leitor.ReadLine();
+                    Console.WriteLine(linha);
                 }
-            };
+            }
+
 
             Console.ReadLine();
-        }
-
-
-        private static void printBuffer(byte[] buffer, int qtdBytesLidos) {
-
-            //Transforma o buffer em string, delimitando o tamanho máximo para leitura
-            var utf8 = Encoding.Default;
-            var texto = utf8.GetString(buffer, 0, qtdBytesLidos);
-
-            Console.Write(texto);
         }
     }
 }

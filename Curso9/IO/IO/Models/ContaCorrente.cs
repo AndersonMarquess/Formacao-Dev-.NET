613f81c6ce8e41853e2098b304b57c9066da25df
AndersonMarquess <andersongabriel96.ag@gmail.com>
2018-11-20 19:34:21 -0200

    Lendo arquivo txt com o FileStream

---
+++ Curso9/IO/IO/Models/ContaCorrente.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace IO.Models
+{
+    class ContaCorrente
+    {
+        public int Agencia { get; private set; }
+        public int Conta { get; private set; }
+
+        public double Saldo { get; private set; }
+
+        public ContaCorrente(int agencia, int conta) {
+            if(agencia < 0 || conta < 0)
+                throw new ArgumentException($"A {Agencia} ou {Conta} não pode ter um valor menor que 0.");
+
+            Agencia = agencia;
+            Conta = conta;
+        }
+
+        public string Sacar(double valor) {
+            if(valor < 0 || valor > Saldo)
+                return "Valor indisponível.";
+
+            Saldo -= valor;
+            return "Saque realizado com sucesso!";
+        }
+
+        public void depositar(double valor) {
+            if(valor < 0)
+                throw new ArgumentException("Valor indisponível.");
+
+            Saldo += valor;
+        }
+
+        public override string ToString() {
+            return "Agencia: "+Agencia+" \nConta: "+Conta+" \nSaldo: "+Saldo;
+        }
+    }
+}
---
+++ Curso9/IO/IO/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IO
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            //Pega o caminho relativo à aplicação, dentro da pasta bin/debug/Dados.txt
+            var filePath = "Dados.txt";
+
+            FileStream fluxoDoArquivo = new FileStream(filePath, FileMode.Open);
+
+            var buffer = new byte[128];
+            var numeroDeBytesLidos = -1;
+
+            while(numeroDeBytesLidos != 0) {
+                numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 128);
+                printBuffer(buffer);
+            }
+
+            Console.ReadLine();
+        }
+
+
+        private static void printBuffer(byte[] buffer) {
+            foreach(var item in buffer) {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}

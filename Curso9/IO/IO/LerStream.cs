37dcb63a7c65b853a57c72d7847ef20de8e83e29
AndersonMarquess <andersongabriel96.ag@gmail.com>
2018-11-22 14:07:23 -0200

    Extraindo métodos para classe LerStream

---
+++ Curso9/IO/IO/LerStream.cs
@@ -0,0 +1,36 @@
+﻿using IO.Models;
+using System;
+using System.IO;
+
+namespace IO
+{
+    class LerStream
+    {
+        public void PrintDadosContaCorrente() {
+            //Pega o caminho relativo à aplicação, dentro da pasta bin/debug/Dados.txt
+            var filePath = "Dados.txt";
+
+
+            using(var fluxoDoArquivo = new FileStream(filePath, FileMode.Open))
+            using(var leitor = new StreamReader(fluxoDoArquivo)) {
+                while(!leitor.EndOfStream) {
+                    var linha = leitor.ReadLine();
+
+                    Console.WriteLine(ParseLinhaParaContaCorrente(linha));
+                    Console.WriteLine("=================");
+                }
+            }
+        }
+
+        public ContaCorrente ParseLinhaParaContaCorrente(string linha) {
+            var dados = linha.Split(',');
+
+            int agencia = Convert.ToInt32(dados[0]);
+            int conta = Convert.ToInt32(dados[1]);
+            double saldo = Convert.ToDouble(dados[2].Replace(".", ","));
+            string titular = dados[3];
+
+            return new ContaCorrente(agencia, conta, titular, saldo);
+        }
+    }
+}
--- Curso9/IO/IO/Program.cs
+++ Curso9/IO/IO/Program.cs
@@ -1,40 +1,13 @@
-﻿using IO.Models;
-using System;
-using System.IO;
-using System.Text;
+﻿using System;
 
 namespace IO
 {
     class Program
     {
         static void Main(string[] args) {
-            //Pega o caminho relativo à aplicação, dentro da pasta bin/debug/Dados.txt
-            var filePath = "Dados.txt";
-
-
-            using(var fluxoDoArquivo = new FileStream(filePath, FileMode.Open))
-            using(var leitor = new StreamReader(fluxoDoArquivo)) {
-                while(!leitor.EndOfStream) {
-                    var linha = leitor.ReadLine();
-
-                    Console.WriteLine(ParseLinhaParaContaCorrente(linha));
-                    Console.WriteLine("=================");
-                }
-            }
-
+            new LerStream().PrintDadosContaCorrente();
 
             Console.ReadLine();
-        }
-
-        private static ContaCorrente ParseLinhaParaContaCorrente(string linha) {
-            var dados = linha.Split(' ');
-
-            int agencia = Convert.ToInt32(dados[0]);
-            int conta = Convert.ToInt32(dados[1]);
-            double saldo = Convert.ToDouble(dados[2].Replace(".",","));
-            string titular = dados[3];
-
-            return new ContaCorrente(agencia, conta, titular, saldo);
         }
     }
 }

c0506e9998b6dc8e2ba91b0dff358ac85d93ace0
AndersonMarquess <andersongabriel96.ag@gmail.com>
2018-11-22 14:57:28 -0200

    Gravando dados binarios

--- Curso9/IO/IO/GravarStream.cs
+++ Curso9/IO/IO/GravarStream.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using IO.Models;
+using System.IO;
 
 namespace IO
 {
@@ -12,6 +13,22 @@ namespace IO
             using(var fluxo = new FileStream(filePath, FileMode.Append))
             using(var writer = new StreamWriter(fluxo)) {
                 writer.Write("123,75464,548.94, Usuário padrão Stream Writer");
+
+                //libera o buffer
+                writer.Flush();
+            }
+        }
+
+
+        public void CriarArquivoBinario() {
+            var filePath = "DadosBinarios.data";
+
+            using(var fluxo = new FileStream(filePath, FileMode.Create))
+            using(var binWriter = new BinaryWriter(fluxo)) {
+                binWriter.Write(123);
+                binWriter.Write(456);
+                binWriter.Write(789.99);
+                binWriter.Write("User default binary Writer");
             }
         }
     }

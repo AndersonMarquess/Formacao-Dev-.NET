a3923a0776b4f7a8519c19e562db06464a7cb0a4
AndersonMarquess <andersongabriel96.ag@gmail.com>
2018-11-23 17:35:31 -0200

    Removendo dados gravados

--- Curso10/EntityFrameworkCore/EntityFrameworkCore/Program.cs
+++ Curso10/EntityFrameworkCore/EntityFrameworkCore/Program.cs
@@ -6,7 +6,9 @@ namespace EntityFrameworkCore
     class Program {
 
         static void Main(string[] args) {
-            //GravarComEntity();
+            GravarComEntity();
+            RecuperarProduto();
+            ExcluirProdutos();
             RecuperarProduto();
 
             Console.ReadLine();
@@ -18,17 +20,34 @@ namespace EntityFrameworkCore
             using(var contexto = new LojaContext()) {
                 //Adiciona o produto
                 contexto.Produtos.Add(p);
+
                 //Salva as alterações
                 contexto.SaveChanges();
             }
         }
 
         private static void RecuperarProduto() {
-            using(var rep = new LojaContext()) {
+            using(var contexto = new LojaContext()) {
                 //Recupera os produtos salvos e retorna uma lista
-                var produtos = rep.Produtos.ToList();
+                var produtos = contexto.Produtos.ToList();
+
+                Console.WriteLine("Foram encontrados {0} produtos.", produtos.Count);
+
                 //Imprime a lista de produtos
                 produtos.ForEach(p => Console.WriteLine(p));
+            }
+        }
+
+        private static void ExcluirProdutos() {
+            using(var contexto = new LojaContext()) {
+                //Recupera lista de produtos
+                var produtos = contexto.Produtos.ToList();
+
+                //Faz a remoção
+                produtos.ForEach(p => contexto.Produtos.Remove(p));
+
+                //Efetiva a alteração
+                contexto.SaveChanges();
             }
         }
     }

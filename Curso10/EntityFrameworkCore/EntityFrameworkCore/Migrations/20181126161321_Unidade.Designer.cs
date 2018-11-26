0e6da8dd65976cc1fdb826c50d7a14030c19a8a8
AndersonMarquess <andersongabriel96.ag@gmail.com>
2018-11-26 14:21:08 -0200

    Alteração na classe produto e atualização da tabela

---
+++ Curso10/EntityFrameworkCore/EntityFrameworkCore/Migrations/20181126161321_Unidade.Designer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using EntityFrameworkCore;
+
+namespace EntityFrameworkCore.Migrations
+{
+    [DbContext(typeof(LojaContext))]
+    [Migration("20181126161321_Unidade")]
+    partial class Unidade
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.6")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("EntityFrameworkCore.Produto", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Categoria");
+
+                    b.Property<string>("Nome");
+
+                    b.Property<double>("PrecoUnitario");
+
+                    b.Property<string>("Unidade");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Produtos");
+                });
+        }
+    }
+}
--- Curso10/EntityFrameworkCore/EntityFrameworkCore/models/Produto.cs
+++ Curso10/EntityFrameworkCore/EntityFrameworkCore/models/Produto.cs
@@ -1,25 +1,32 @@
-﻿namespace EntityFrameworkCore
+﻿/// <summary>
+/// Para sincronizar a classe com a tabela:
+///
+/// Add-Migration NomeDaMigracao
+/// Update-Database
+/// 
+/// </summary>
+namespace EntityFrameworkCore
 {
     internal class Produto
     {
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Categoria { get; set; }
-        public double Preco { get; set; }
+        public double PrecoUnitario { get; set; }
         public string Unidade { get; set; }
 
-        public Produto(string nome, string categoria, double preco) {
+        public Produto(string nome, string categoria, double precoUnitario) {
             Nome = nome;
             Categoria = categoria;
-            Preco = preco;
+            PrecoUnitario = precoUnitario;
         }
 
         public Produto() {
         }
 
-        //Preco:C2 => R$ 9,90
+        //PrecoUnitario:C2 => R$ 9,90
         public override string ToString() {
-            return $"Id: {Id} - Categoria: {Categoria} - Nome: {Nome} - Preço: {Preco:C2}";
+            return $"Id: {Id} - Categoria: {Categoria} - Nome: {Nome} - Preço: {PrecoUnitario:C2}";
         }
     }
 }

b4c6999061520932bbce98796043b0b3fab1f9ec
AndersonMarquess <andersongabriel96.ag@gmail.com>
2019-01-09 17:42:31 -0200

    Criado projeto do curso 6
    Criado método para pegar e imprimir informações com substring

---
+++ Curso6/String_Regex/String_Regex/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Regex {
+    class Program {
+        static void Main(string[] args) {
+            tratarStringPadrao();
+
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Pega a string a partir do index 7 e imprime o resultado.
+        /// 
+        /// Texto padrão = "pagina?argumentos"
+        /// Resultado = argumentos
+        /// </summary>
+        private static void tratarStringPadrao() {
+            string url = "pagina?argumentos";
+            var resultado = url.Substring(7);
+            Console.WriteLine("Resultado da substring: " + resultado);
+        }
+    }
+}

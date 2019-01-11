c8086da89267bf549a6474f4ea9d68829e7b6f0d
AndersonMarquess <andersongabriel96.ag@gmail.com>
2019-01-11 12:02:31 -0200

    Criado método para trabalhar com regex e validar telefone

--- Curso6/String_Regex/String_Regex/Program.cs
+++ Curso6/String_Regex/String_Regex/Program.cs
@@ -1,11 +1,29 @@
 ﻿using String_Regex.util;
 using System;
+using System.Text.RegularExpressions;
 
 namespace String_Regex {
     class Program {
         static void Main(string[] args) {
             //tratarStringPadrao();
+            //extrairValorUrlPadrao();
+            usandoRegex();
 
+
+            Console.ReadLine();
+        }
+
+        private static void usandoRegex() {
+            string dado = "Meu número de telefone é: 11-91234-5698";
+            string padraoTelefone = "[0-9][0-9]-[0-9][0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]";
+            Console.WriteLine("O Padrão existe: "+Regex.IsMatch(dado, padraoTelefone));
+            Console.WriteLine("Padrão recuperado: "+Regex.Match(dado, padraoTelefone));
+        }
+
+        /// <summary>
+        /// Com base no nome do parâmetro, recupera o valor presente na url.
+        /// </summary>
+        private static void extrairValorUrlPadrao() {
             var url = @"http://www.site.com/pagina?
                             arg1=hEllo
                            &arg2=WorlD
@@ -13,8 +31,6 @@ namespace String_Regex {
 
             UrlHandler urlHandler = new UrlHandler(url);
             Console.WriteLine(urlHandler.getValor("ArG3"));
-
-            Console.ReadLine();
         }
 
         /// <summary>
